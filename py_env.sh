77f6881d40fb705ad96af2559dd97e1fefdaeb3f
zhen.shen <zhen.shen@yamu.com>
2025-08-25 09:53:45 +0800

    更新环境安装脚本

--- py_env.sh
+++ py_env.sh
@@ -1,8 +1,13 @@
-pip install openai
-pip install langchain
-pip install langchain
-pip install langchain-community
-pip install langchain-experimental
-pip install "langserve[all]"
-pip install langchain-cli
-pip install langsmith
+#!/bin/bash
+
+# 定义 pip 命令变量
+PIP_CMD=pip3  # 可以根据需要改为 pip
+
+# 使用变量安装包
+$PIP_CMD install openai
+$PIP_CMD install langchain
+$PIP_CMD install langchain-community
+$PIP_CMD install langchain-experimental
+$PIP_CMD install "langserve[all]"
+$PIP_CMD install langchain-cli
+$PIP_CMD install langsmith
